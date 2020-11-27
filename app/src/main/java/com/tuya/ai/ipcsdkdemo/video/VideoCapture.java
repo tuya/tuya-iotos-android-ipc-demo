5f7159f807d366d18ae17d9250aea7d9aaa42553
changyue <zhangrd@tuya.com>
2020-11-27 15:06:31 +0800

    fix: demo preview black & add device config

--- app/src/main/java/com/tuya/ai/ipcsdkdemo/video/VideoCapture.java
+++ app/src/main/java/com/tuya/ai/ipcsdkdemo/video/VideoCapture.java
@@ -3,8 +3,15 @@ package com.tuya.ai.ipcsdkdemo.video;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.view.SurfaceHolder;
+
+import com.tuya.smart.aiipc.ipc_sdk.api.Common;
+import com.tuya.smart.aiipc.ipc_sdk.api.IParamConfigManager;
+import com.tuya.smart.aiipc.ipc_sdk.service.IPCServiceManager;
+import com.tuya.smart.aiipc.netconfig.ConfigProvider;
 
 import java.io.IOException;
+import java.util.List;
 
 public class VideoCapture {
 
@@ -13,10 +20,12 @@ public class VideoCapture {
     VideoCodec mCodec;
     private SurfaceTexture mSurfaceTexture;
 
+    private int mChannel;
+
     public VideoCapture(int channel) {
 
         pixelBuffer = new byte[1280 * 720 * 3 / 2];
-        mCodec = new VideoCodec(channel);
+        mCodec = new VideoCodec(mChannel = channel);
 
     }
     public void startVideoCapture() {
@@ -39,16 +48,62 @@ public class VideoCapture {
         p.setPreviewFpsRange(30000, 30000);
         p.setPreviewSize(1280, 720);
 
-        mCamera.setParameters(p);
-        mSurfaceTexture = new SurfaceTexture(0);
-
         try {
-            mCamera.setPreviewTexture(mSurfaceTexture);
+            if (ConfigProvider.getConfig(ConfigProvider.QR_OUTPUT) instanceof SurfaceHolder) {
+                mCamera.setPreviewDisplay((SurfaceHolder) ConfigProvider.getConfig(ConfigProvider.QR_OUTPUT));
+                ConfigProvider.setConfig(ConfigProvider.QR_OUTPUT, null);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-            mCamera.addCallbackBuffer(pixelBuffer);
+        IParamConfigManager configManager = IPCServiceManager.getInstance().getService(IPCServiceManager.IPCService.MEDIA_PARAM_SERVICE);
+        int configRate = configManager.getInt(mChannel, Common.ParamKey.KEY_VIDEO_FRAME_RATE);
+
+//        Optional<int[]> first = p.getSupportedPreviewFpsRange().stream()
+//                .filter(ints -> ints.length == 2 && ints[0] == ints[1] && ints[0] == configRate * 1000).findFirst();
+//
+//        if (first.isPresent()) {
+//            p.setPreviewFpsRange(first.get()[0], first.get()[0]);
+//        } else {
+//            p.setPreviewFpsRange(24000, 24000);
+//        }
+
+        List<int[]> supported = p.getSupportedPreviewFpsRange();
+        boolean isFound = false;
+        for (int[] fps : supported){
+            if (fps.length == 2 && fps[0] == fps[1] && fps[0] == configRate * 1000) {
+                isFound = true;
+                break;
+            }
+        }
+
+        if (isFound){
+            p.setPreviewFpsRange(configRate * 1000, configRate * 1000);
+        }else{
+            p.setPreviewFpsRange(supported.get(supported.size()-1)[0], supported.get(supported.size()-1)[1]);
+        }
+
+
+//        p.setPreviewFpsRange(30000, 30000);
+//        for (int[] ints : p.getSupportedPreviewFpsRange()) {
+//            Log.w("Preview", "range");
+//            for (int i : ints) {
+//                Log.w("Preview", "range: " + i);
+//            }
+//        }
+
+        mCamera.setParameters(p);
+        mSurfaceTexture = new SurfaceTexture(0);
+
+//        try {
+//            mCamera.setPreviewTexture(mSurfaceTexture);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        mCamera.addCallbackBuffer(pixelBuffer);
+
         mCamera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
             @Override
             public void onPreviewFrame(byte[] data, Camera camera) {
